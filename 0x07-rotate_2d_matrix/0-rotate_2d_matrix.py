946e307f8fe3c56d49fd18d83f91f10175adafa3
Ayodele Kadiri <kadelcode@gmail.com>
2023-09-14 07:19:25 -0700

    change algorithm

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 """
-Rotate 2D Matrix
+Rotate Matrix
 """
+
 
 def rotate_2d_matrix(matrix):
     """
-    A fuction that rotates an n x n matrix
-    by 90 degrees.
+    A function that rotates a matrix
+    by 90 degrees
     """
 
-    # Transpose the matrix
-    transposed_matrix = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
-    print(transposed_matrix)
+    n = len(matrix)
 
-    # Reverse each row of the transposed matrix.
-    for i in transposed_matrix:
-        i.reverse()
+    # Reverse each row of the matrix.
+    for i in range(n // 2):
+        for j in range(n):
+            matrix[i][j], matrix[n - 1 - i][j] =\
+                matrix[n - 1 - i][j], matrix[i][j]
 
-    # Return the rotated matrix.
-    # return transposed_matrix
+    # Transpose the matrix.
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
